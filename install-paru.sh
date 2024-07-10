73f590ae1df18dedf9057f1d22939f68fb38b4c3
amaliegay <amaliegay@outlook.com>
2024-07-10 10:28:51 +0800

    windows

--- install-paru.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -si
-rm -rf paru
--- install-slock.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-git clone https://git.suckless.org/slock $HOME/slock
-cp slock/config.def.h $HOME/slock
-cd $HOME/slock
-sudo make install
-cd ..
-rm -rf slock
